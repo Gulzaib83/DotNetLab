275aa90c3cc43c2e86921811d9cb4d7baf456a28
Gulzaib Khan <gulzaib.khan@codeninjaconsulting.com>
2023-09-28 13:01:01 +0500

    Task-4 (Extras)

---
+++ AWSLambdaAPI/Models/AWS_ToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace AWSLambdaAPI.Models
+{
+    public class AWS_ToDo
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public bool? IsCompleted { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
